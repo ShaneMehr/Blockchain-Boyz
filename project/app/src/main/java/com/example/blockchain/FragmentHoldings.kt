a010f2c025aa93f4f837f070b6307a7da70d96c2
Shane Mehr <shanemehr@gmail.com>
2022-05-04 19:19:03 -0400

    Button on click functionality for holdings

--- project/app/src/main/java/com/example/blockchain/FragmentHoldings.kt
+++ project/app/src/main/java/com/example/blockchain/FragmentHoldings.kt
@@ -1,38 +1,27 @@
 package com.example.blockchain
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class FragmentHoldings : Fragment() {
 
-    @SuppressLint("UseRequireInsteadOfGet")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragmentholdings_layout, container, false)
-    }
-
-    fun onClickBuy() {
-        val buyBTN = requireView().findViewById<Button>(R.id.holdingBuy)
-        buyBTN?.setOnClickListener({
+        val newView = inflater.inflate(R.layout.fragmentholdings_layout, container, false)
+        val buyButton = newView.findViewById<Button>(R.id.holdingBuy)
+        buyButton.setOnClickListener {
             val usdView = view?.findViewById<TextView>(R.id.holdingUSD)
             usdView?.text = "$100"
-        })
+        }
+        return newView
     }
 
 }
