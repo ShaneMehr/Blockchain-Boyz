2404c2bf567ab235a553f01ebea79203bd6a5976
Jacob James <jacobredfort@gmail.com>
2022-05-05 17:23:43 -0400

    News so far

--- project/app/src/main/java/com/example/blockchain/FragmentNews.kt
+++ project/app/src/main/java/com/example/blockchain/FragmentNews.kt
@@ -6,13 +6,38 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.blockchain.R
+import android.os.AsyncTask
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.io.IOException
+import java.io.InputStream
+import java.lang.ref.WeakReference
+import java.net.HttpURLConnection
+import java.net.URL
 
 class FragmentNews : Fragment() {
+
+    private var columnCount = 1
+
+    private var listener: OnListFragmentInteractionListener? = null
+
+    val RSS_FEED_LINK = "https://www.relsellglobal.in/feed/";
+
+    var adapter: MyItemRecyclerViewAdapter? = null
+    var rssItems = ArrayList<RssItem>()
+
+    var listV : RecyclerView ?= null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragmentnews_layout, container, false)
+    }
+
+    interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onListFragmentInteraction(item: RssItem?)
     }
 }
---
+++ project/app/src/main/java/com/example/blockchain/RssItem.kt
@@ -0,0 +1,13 @@
+package com.example.blockchain
+
+class RssItem {
+        var title = ""
+        var link = ""
+        var pubDate = ""
+        var description = ""
+        var category = ""
+
+        override fun toString(): String {
+            return "RssItem(title='$title', link='$link', pubDate='$pubDate', description='$description', category='$category')"
+        }
+}
